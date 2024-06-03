f7b3fb6d9dde336c3953655eb27af613556bbc26
Christoph Hartmann <chris@lollyrock.com>
2024-06-03 11:38:52 +0200

    Update description & documentation
    Co-authored-by: Tim Smith <tsmith84@gmail.com>

--- providers/defaults.go
+++ providers/defaults.go
@@ -239,7 +239,7 @@ var DefaultProviders Providers = map[string]*Provider{
 				{
 					Name:  "nmap",
 					Use:   "nmap",
-					Short: "a nmap network scanner",
+					Short: "a Nmap network scanner",
 				},
 			},
 		},
